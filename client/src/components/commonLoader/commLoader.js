7f67c9e68d0afffd9ebedcc15992d12e29c7cfe7
ghoshariji <144580430+ghoshariji@users.noreply.github.com>
2024-11-22 16:25:32 +0530

    push

---
+++ client/src/components/commonLoader/commLoader.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./commLoader.css"
+const commLoader = () => {
+  return (
+    <div>
+      <div>
+        <div class="loader"></div>
+      </div>
+    </div>
+  );
+};
+
+export default commLoader;
